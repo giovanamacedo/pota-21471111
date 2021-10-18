ed039325dd1a9d8d95e42cad4766c003b2013fa0
giovanamacedo <giovanaml12@gmail.com>
2021-10-18 17:28:27 -0300

    leitura do arquivo de dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            // arquivo com dados de entrada
+            File file = new File("arquivoDados.txt");
+            Scanner scanner = new Scanner(file);
 
+            // leitura dos dados do cliente
+            while (scanner.hasNextLine()) {
+                // leia a próxima linha do arquivo
+                String linha = scanner.nextLine();
+
+                // quebrando a linha por ","
+                String[] partes = linha.split(",");
+
+                // exibindo cada dados dos clientes
+                for (int i = 0; i < partes.length; i++) {
+                    System.out.print(partes[i] + " - ");
+                }
+                System.out.println();
+
+            }
+
+            scanner.close();
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
