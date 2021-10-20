ec3ebde34e6dfb10102a6c1e192a9591aa2ea05c
giovanamacedo <giovanaml12@gmail.com>
2021-10-20 15:57:20 -0300

    adicionando construtor e getter

--- src/Clientes.java
+++ src/Clientes.java
@@ -1,2 +1,25 @@
 public class Clientes {
+
+    private String nome;
+    private String sexo;
+    private String endereco;
+    private String cidade;
+    private String email;
+    private String telefone;
+    private int idade;
+
+    public Clientes(String nome, String sexo, String endereco, String cidade, String email, String telefone, int idade) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.email = email;
+        this.telefone = telefone;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 }
